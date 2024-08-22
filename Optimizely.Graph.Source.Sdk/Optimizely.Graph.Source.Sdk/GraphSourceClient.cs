77c08606de4390abca73de425f0020aa6989e1cc
jakeminard <minardjake@gmail.com>
2024-08-22 09:44:37 -0400

    Minor refactor.

--- Optimizely.Graph.Source.Sdk/Optimizely.Graph.Source.Sdk/GraphSourceClient.cs
+++ Optimizely.Graph.Source.Sdk/Optimizely.Graph.Source.Sdk/GraphSourceClient.cs
@@ -6,11 +6,11 @@ namespace Optimizely.Graph.Source.Sdk
 {
     public class GraphSourceClient
     {
-        internal IGraphSourceRepository _repository;
+        internal IGraphSourceRepository repository;
 
         private GraphSourceClient(IGraphSourceRepository repository)
         {
-            this._repository = repository;
+            this.repository = repository;
         }
 
         /// <summary>
@@ -23,42 +23,42 @@ namespace Optimizely.Graph.Source.Sdk
         /// <returns></returns>
         public static GraphSourceClient Create(string baseUrl, string source, string appKey, string secret)
         {
-            IContentGraphClientFactory contentGraphClientFactory = new ContentGraphClientFactory(baseUrl, source, appKey, secret);
+            var contentGraphClientFactory = new ContentGraphClientFactory(baseUrl, source, appKey, secret);
             var repository = new GraphSourceRepository(contentGraphClientFactory.Create());
             return new GraphSourceClient(repository);
         }
 
         public void AddLanguage(string language)
         {
-            _repository.AddLanguage(language);
+            repository.AddLanguage(language);
         }
 
         public SourceConfigurationModel<T> ConfigureContentType<T>()
             where T : class, new()
         {
-            return _repository.ConfigureContentType<T>();
+            return repository.ConfigureContentType<T>();
         }
 
         public SourceConfigurationModel<T> ConfigurePropertyType<T>()
             where T : class, new()
         {
-            return _repository.ConfigurePropertyType<T>();
+            return repository.ConfigurePropertyType<T>();
         }
 
         public async Task<string> SaveTypesAsync()
         {
-            return await _repository.SaveTypesAsync();
+            return await repository.SaveTypesAsync();
         }
 
         public async Task<string> SaveContentAsync<T>(Func<T, string> generateId, params T[] data)
             where T : class, new()
         {
-            return await _repository.SaveContentAsync(generateId, data);
+            return await repository.SaveContentAsync(generateId, data);
         }
 
         public async Task<string> DeleteContentAsync(string id)
         {
-            return await _repository.DeleteContentAsync(id);
+            return await repository.DeleteContentAsync(id);
         }
     }
 }
