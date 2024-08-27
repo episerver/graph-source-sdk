cda2a852611032753364b8f2acb09469a3aedf58
jakeminard <minardjake@gmail.com>
2024-08-27 10:52:31 -0400

    gitignore.

--- Optimizely.Graph.Source.Sdk/Optimizely.Graph.Source.Sdk.Tests/obj/Debug/net8.0/Optimizely.Graph.Source.Sdk.Tests.AssemblyInfo.cs
+++ Optimizely.Graph.Source.Sdk/Optimizely.Graph.Source.Sdk.Tests/obj/Debug/net8.0/Optimizely.Graph.Source.Sdk.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Optimizely.Graph.Source.Sdk.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e1c607173351b446e321a5c0a880d504afe2783")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783272f787a02ef78a3a640929f475b17ed8329f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Optimizely.Graph.Source.Sdk.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Optimizely.Graph.Source.Sdk.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
